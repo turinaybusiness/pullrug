ca851cdc775639b2c63d7a22e3af6b5e0ba01a45
musevibe <turinaybusiness@gmail.com>
2024-12-02 21:43:17 +0300

    tet

--- api/store-mint.js
+++ api/store-mint.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     );
 
     res.status(200).json({
-      message: "Address stored successfully",
+      message: "Thank you for helping train our model!",
       data: result.rows[0], // Return the inserted row
     });
   } catch (error) {
